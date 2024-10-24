5b1768cc0fbc91f1ec0c048e8f101c34dd6816ba
Mauricio Santos <mauriciosantosnetto@gmail.com>
2024-10-23 21:41:47 -0300

    feat: faz toda a configuração inicial, instala pacotes de dependências e configura a conexão com o banco de dados

---
+++ PicPaySimplificado/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PicPaySimplificado.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
